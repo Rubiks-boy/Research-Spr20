db6ad77b8e0c2b03671181e3703b43c6e11f3580
Adam Walker <adamwalker567123@gmail.com>
2020-02-13 15:14:39 -0800

    Loading and saving params

---
+++ Heavy spr, rounded latch, const F/saveParams.m
@@ -0,0 +1,14 @@
+clearvars;
+close all;
+
+p = struct;
+% p.m = logspace(-5, 5, 100);
+p.m = 5;
+p.m_spr = 0.5;
+p.v0 = 1;
+p.F_l = 0;
+p.m_l = 1000000;
+p.R = 8;
+p.t = linspace(0, 6, 1000);
+
+save('params', '-struct', 'p');
