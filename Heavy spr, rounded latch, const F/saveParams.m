73f7c354b122a25bca03f0314b43995653f2242d
Adam Walker <adamwalker567123@gmail.com>
2020-02-13 18:29:02 -0800

    plot good, except for discontinuity whwere t_l becomes 0

--- Heavy spr, rounded latch, const F/saveParams.m
+++ Heavy spr, rounded latch, const F/saveParams.m
@@ -2,13 +2,13 @@ clearvars;
 close all;
 
 p = struct;
-p.m = logspace(-5, 5, 100);
+p.m = logspace(-5, 5, 1000);
 % p.m = 5;
 p.m_spr = 0.5;
 p.v0 = 1;
 p.F_l = 0;
 p.m_l = 1000000;
-p.R = 8;
+p.R = 3;
 p.t = linspace(0, 10, 1000);
 
 save('params', '-struct', 'p');
