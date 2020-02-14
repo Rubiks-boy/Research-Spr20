e5ed130a19c7d4072fb996abc5ec209dadd0b297
Adam Walker <adamwalker567123@gmail.com>
2020-02-14 15:59:28 -0800

    labeled plots, plots for kinematics for all time

--- Heavy spr, rounded latch, const F/saveParams.m
+++ Heavy spr, rounded latch, const F/saveParams.m
@@ -2,13 +2,13 @@ clearvars;
 close all;
 
 p = struct;
-p.m = logspace(-5, 5, 1000);
+p.m = logspace(-5, 5, 100);
 % p.m = 5;
 p.m_spr = 0.5;
 p.v0 = 1;
 p.F_l = 0;
 p.m_l = 1000000;
 p.R = 3;
-p.t = linspace(0, 10, 1000);
+p.t = linspace(0, 5, 1000); % MUST start at 0
 
 save('params', '-struct', 'p');
