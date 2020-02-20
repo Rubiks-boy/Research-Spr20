21108df428fc9e6f113d23ba26d957f94a8a37bc
Adam Walker <adamwalker567123@gmail.com>
2020-02-20 12:52:05 -0800

    fixup! Params for ideal latch, and rounded latch

--- Heavy spr, rounded latch, const F/saveParams.m
+++ Heavy spr, rounded latch, const F/saveParams.m
@@ -16,7 +16,7 @@ p1.t_perc_above = 1.2; % How far above t_to + t_l to find values for p1rojectile
 %% params for slowly removed rounded latch (like pg 6 sup1p1lemental)
 p2 = struct;
 p2.m = logspace(-5, 5, 500);
-p2.m_sp2r = 0.5;
+p2.m_spr = 0.5;
 p2.v0 = 0.2;
 p2.F_l = 0;
 p2.m_l = 1000000;
@@ -25,4 +25,4 @@ p2.num_times = 1000;
 p2.t = linspace(0, 10, p2.num_times); % MUST start at 0
 p2.t_perc_above = 1.2; % How far above t_to + t_l to find values for p1rojectile motion
 
-save('params', '-struct', 'p1');
+save('params', '-struct', 'p2');
