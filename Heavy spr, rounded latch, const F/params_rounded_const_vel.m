53076b77151ced72a2f7c954b3811ab4fb7d3332
Adam Walker <adamwalker567123@gmail.com>
2020-02-20 13:08:01 -0800

    Params in separate file

--- Heavy spr, rounded latch, const F/params_rounded_const_vel.m
+++ Heavy spr, rounded latch, const F/params_rounded_const_vel.m
@@ -1,15 +1,16 @@
 clearvars;
 close all;
 
+%% params for slowly removed rounded latch (like pg 6 sup1p1lemental)
 p = struct;
 p.m = logspace(-5, 5, 500);
 p.m_spr = 0.5;
-p.v0 = 0.25;
+p.v0 = 0.05;
 p.F_l = 0;
 p.m_l = 1000000;
-p.R = 0.0001;
+p.R = 0.5;
 p.num_times = 1000;
 p.t = linspace(0, 10, p.num_times); % MUST start at 0
-p.t_perc_above = 1.2; % How far above t_to + t_l to find values for projectile motion
+p.t_perc_above = 1.2; % How far above t_to + t_l to find values for p1rojectile motion
 
 save('params', '-struct', 'p');
